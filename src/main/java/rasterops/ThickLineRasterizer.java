04828d48feb76aaa3b7ab17763517a51bffaa3b9
KseniyaBabaryka-creator <kseniabobariko2005@gmail.com>
2024-10-20 23:09:39 +0200

    Bonus task for thin line added

--- src/main/java/rasterops/ThickLineRasterizer.java
+++ src/main/java/rasterops/ThickLineRasterizer.java
@@ -13,14 +13,44 @@ public class ThickLineRasterizer extends LineRasterizer{
 
         double length = Math.sqrt(vc * vc + vr * vr);
 
-        double nc = -vr / length;
-        double nr = -vc / length;
+//        double nc = -vr / length;
+//        double nr = -vc / length;
 
-        for (int i = -thickness/ 2; i <= thickness ; i++) {
+        double nc = -vr / length * thickness / 2;
+        double nr = -vc / length * thickness / 2;
+
+        for (int i = -thickness/ 2; i <= thickness / 2 ; i++) {
             int offsetX = (int) (i * nc);
             int offsetY = (int) (i * nr);
 
             new FilledLineRasterizer().drawLine(raster, c1 + offsetX, r1 + offsetY, c2 + offsetX, r2 + offsetY, color);
+        }
+
+        drawCircle(raster, c1, r1, thickness / 2, color);
+        drawCircle(raster, c2, r2, thickness / 2, color);
+    }
+
+    public void drawCircle(RasterBufferedImage raster, int cx, int cy, int r, int color){
+        int x = r;
+        int y = 0;
+        int error = 0;
+
+        while(x >= y) {
+            raster.setColor(cx + x, cy + y, color);
+            raster.setColor(cx + y, cy + x, color);
+            raster.setColor(cx - y, cy + x, color);
+            raster.setColor(cx - x, cy + y, color);
+            raster.setColor(cx - x, cy - y, color);
+            raster.setColor(cx - y, cy - x, color);
+            raster.setColor(cx + y, cy - x, color);
+            raster.setColor(cx + x, cy - y, color);
+
+            y++;
+            error += 1 + 2 * y;
+            if (2 * (error - x) + 1 > 0){
+                x--;
+                error += 1 - 2 * x;
+            }
         }
     }
 }
